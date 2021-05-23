1e548cfe1e622a06f6ca1e4f9c9637b4a4c4214b
devdattakhoche <dkhoche70@gmail.com>
2021-05-23 12:13:02 +0530

    black Fix

--- leads/migrations/0002_rename_leads_lead.py
+++ leads/migrations/0002_rename_leads_lead.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('leads', '0001_initial'),
+        ("leads", "0001_initial"),
     ]
 
     operations = [
         migrations.RenameModel(
-            old_name='Leads',
-            new_name='Lead',
+            old_name="Leads",
+            new_name="Lead",
         ),
     ]
