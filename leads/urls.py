86d81b9e1c9baaf7480be88a5646785bb963af3b
devdattakhoche <dkhoche70@gmail.com>
2021-05-24 11:29:38 +0530

    Added Styles

--- leads/urls.py
+++ leads/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "leads"
 
 urlpatterns = [
-    path("", views.lead_list, name="lead_list"),
-    path("<int:id>/", views.lead_detail, name="lead_detail"),
-    path("create/", views.lead_create, name="lead_create"),
-    path("<int:id>/update/", views.lead_update, name="lead_update"),
-    path("<int:id>/delete/", views.lead_delete, name="lead_delete"),
+    path("", views.LeadListView.as_view(), name="lead_list"),
+    path("<int:pk>/", views.LeadDetailView.as_view(), name="lead_detail"),
+    path("create/", views.LeadCreateView.as_view(), name="lead_create"),
+    path("<int:pk>/update/", views.LeadUpdateView.as_view(), name="lead_update"),
+    path("<int:pk>/delete/", views.lead_delete, name="lead_delete"),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from .models import Agent, Lead
+from .models import Lead
 from .forms import LeadModelForm, Leadform
+from django.views import generic
+from django.urls import reverse
+from django.core.mail import send_mail
+
+
+class LandingView(generic.TemplateView):
+    template_name = "landing.html"
+
+
+def landing_page(request):
+    return render(request, "landing.html")
+
+
+class LeadListView(generic.ListView):
+    template_name = "leads/lead_list.html"
+    context_object_name = "leads"  # default object name 'object_list'
+    model = Lead  # pass model or queryset
+    # queryset = Lead.objects.all()
 
 
 def lead_list(request):
@@ -10,10 +27,36 @@ def lead_list(request):
     return render(request, "leads/lead_list.html", params)
 
 
-def lead_detail(request, id):
-    lead = Lead.objects.get(id=id)
+class LeadDetailView(generic.DetailView):
+    # default pk is needed in url
+    template_name = "leads/lead_detail.html"
+    context_object_name = "lead"
+    model = Lead  # pass model or queryset
+    # queryset = Lead.objects.all()
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
     params = {"lead": lead}
     return render(request, "leads/lead_detail.html", params)
+
+
+class LeadCreateView(generic.CreateView):
+    model = Lead
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+
+    def form_valid(self, form):
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"],
+        )
+        return super(LeadCreateView, self).form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse("leads:lead_list")
 
 
 def lead_create(request):
@@ -26,8 +69,17 @@ def lead_create(request):
     return render(request, "leads/lead_create.html", params)
 
 
-def lead_update(request, id):
-    lead = Lead.objects.get(id=id)
+class LeadUpdateView(generic.UpdateView):
+    model = Lead
+    form_class = LeadModelForm
+    template_name = "leads/lead_update.html"
+
+    def get_success_url(self) -> str:
+        return reverse("leads:lead_list")
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
 
     if request.method == "POST":
         form = LeadModelForm(request.POST, instance=lead)
@@ -38,11 +90,20 @@ def lead_update(request, id):
     return render(request, "leads/lead_update.html", params)
 
 
-def lead_delete(request, id):
-    lead = Lead.objects.get(id=id)
+class LeadDeleteView(generic.DeleteView):
+    model = Lead
+    # Delete view by default gives us a form to submit , so we can mention it in template
+    def get_success_url(self) -> str:
+        return reverse("leads:lead_list")
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
     lead.delete()
     return redirect("leads:lead_list")
 
+
+## FORMS
 
 # def lead_update(request, id):
 #     lead = Lead.objects.get(id=id)
